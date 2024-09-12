7fdf9b6678397f6a9179e0c9fa53db33761552d5
Hiroaki Kawata <hiroaki.kawata@engilico.com>
2024-09-12 23:24:52 +0200

    Modify init.lua

--- init.lua
+++ init.lua
@@ -83,6 +83,7 @@ I hope you enjoy your Neovim journey,
 
 P.S. You can delete this when you're done too. It's your config now! :)
 --]]
+vim.env.LANG = 'en_US.UTF-8'
 
 -- Set <space> as the leader key
 -- See `:help mapleader`
@@ -91,7 +92,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
